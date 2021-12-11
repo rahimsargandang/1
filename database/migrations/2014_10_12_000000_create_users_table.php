2ea02a231b77cdb10c30d7837857e145dcc3a5e2
fahmiiinoh <a178796@siswa.ukm.edu.my>
2021-12-12 00:06:09 +0800

    test push

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('voter', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('namaaaaaaaaaaaaaae');
             $table->string('matricnum')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
