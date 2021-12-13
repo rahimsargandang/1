5ee6600d48ff0ab8c247ed5c0cb38ef34bf54583
Nur Suhadah Binti Nooranizam <A182511@siswa.ukm.edu.my>
2021-12-13 21:32:06 +0800

    Update application form

--- resources/views/applications/index.blade.php
+++ resources/views/applications/index.blade.php
@@ -11,27 +11,32 @@
 </head>
 <body>
 <form>
-<div class="container">
+  <div class="container">
+  <div class="row justify-content-center">
+        <div class="col-md-8">
+<div class="card">
+<div class="card-header">{{ __('Candidate Application') }}</div>
     <div class="row justify-content-center ">
         <div class="col-md-5">
 
-  <div class="form-group">
+  <div class="form-group pt-2">
     <label for="candidateapplicationname">Name</label>
+    <br>
     <input type="text" class="form-control" id="candidateapplicationname"  placeholder="Name">
   </div>
-  <div class="form-group">
+  <div class="form-group pt-2">
     <label for="candidateapplicationmatricnum">Matric Number</label>
     <input type="text" class="form-control" id="candidateapplicationmatricnum"  placeholder="Matric Number">
   </div>
-  <div class="form-group">
+  <div class="form-group pt-2">
     <label for="candidatestrength">Strength</label>
-    <input type="text" style="height:100px" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" >
+    <textarea class="form-control" style="" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" rows="3" ></textarea>
   </div>
-  <div class="form-group">
+  <div class="form-group pt-2">
     <label for="candidatecgpa">CGPA</label>
     <input type="text" class="form-control" id="candidatecgpa" placeholder="CGPA">
   </div>
-  <div class="form-group">
+  <div class="form-group pt-2">
                             <label class="my-1 me-2" for="country">Faculty</label>
                             <select class="form-select" id="country" aria-label="Default select example">
                                 <option selected>Select Faculty</option>
@@ -40,10 +45,13 @@
                                 <option value="3">FEB</option>
                             </select>
                         </div>
-                        <div class="row mb-6 pt-2" >
+                        <div class="row mb-6 pt-2 pb-2" >
                             <div class="col-md-8 offset-md-5">
   <button type="submit" class="btn btn-primary">Submit</button>
   </div>
+</div>
+</div>
+</div>
 </div>
   
 </div>
