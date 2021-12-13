139148b8a7a95bdd0b41bf5eff311e7833cbd845
fahmiiinoh <a178796@siswa.ukm.edu.my>
2021-12-13 18:48:37 +0800

    Aplication UI updated

--- resources/views/applications/index.blade.php
+++ resources/views/applications/index.blade.php
@@ -6,25 +6,49 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Test</title>
+    <title>Candidate Application Form</title>
 
 </head>
 <body>
 <form>
+<div class="container">
+    <div class="row justify-content-center ">
+        <div class="col-md-5">
+
   <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+    <label for="candidateapplicationname">Name</label>
+    <input type="text" class="form-control" id="candidateapplicationname"  placeholder="Name">
   </div>
   <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+    <label for="candidateapplicationmatricnum">Matric Number</label>
+    <input type="text" class="form-control" id="candidateapplicationmatricnum"  placeholder="Matric Number">
   </div>
-  <div class="form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  <div class="form-group">
+    <label for="candidatestrength">Strength</label>
+    <input type="text" style="height:100px" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" >
   </div>
+  <div class="form-group">
+    <label for="candidatecgpa">CGPA</label>
+    <input type="text" class="form-control" id="candidatecgpa" placeholder="CGPA">
+  </div>
+  <div class="form-group">
+                            <label class="my-1 me-2" for="country">Faculty</label>
+                            <select class="form-select" id="country" aria-label="Default select example">
+                                <option selected>Select Faculty</option>
+                                <option value="1">FTSM</option>
+                                <option value="2">FSK</option>
+                                <option value="3">FEB</option>
+                            </select>
+                        </div>
+                        <div class="row mb-6 pt-2" >
+                            <div class="col-md-8 offset-md-5">
   <button type="submit" class="btn btn-primary">Submit</button>
+  </div>
+</div>
+  
+</div>
+</div>
+</div>
 </form>
 </body>
 </html>
