4aa102e49e8b7ab096a86fbfa8bf9a73334b28a9
wkwbh <a182566@siswa.ukm.edu.my>
2022-01-03 21:41:04 +0800

    search view

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,21 +1,37 @@
 @extends('navbar')
 
 @section('content')
-<div class="container">
-
-    <div class="row">
-        <div class="text-center">
-           
-            search
-
-        </div>
-        <div class="col-9">
-
-
-        </div>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Search with pagination</title>
+    <link rel="stylesheet" href="{{ asset('bootstrap-3.1.1/css/bootstrap.min.css')}}">
+</head>
+<body>
+    <div class="container">
+       <div class="row">
+          <div class="col-md-6" style="margin-top:40px">
+             <h4>Search Everything</h4><hr>
+             <form action="{{ route('search') }}" method="GET">
+        
+                <div class="form-group">
+                   <label for="">Enter keyword</label>
+                   <input type="text" class="form-control" name="query" placeholder="Search here....." value="{{ request()->input('query') }}">
+                   <br>
+                   <span class="text-danger">@error('query'){{ $message }} @enderror</span>
+                </div>
+                <div class="form-group">
+                 <button type="submit" class="btn btn-primary">Search</button>
+                </div>
+             </form>
+          </div>
+       </div>
     </div>
-</div>
+</body>
+</html>
 @endsection          
 
 
